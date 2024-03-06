ea6cedee7f27dec5299c060dd13260455163779a
Arpit Mehndiratta <arpitmehandiratta8@gmail.com>
2024-03-06 20:01:42 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+const HeroSection = () => {
+  return (
+    <main className="hero container">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+        Experience unparalleled comfort with our carefully crafted designs that prioritize both style and support.Our shoes will keep you looking and feeling your best..
+        </p>
+
+        <div className="hero-btn">
+          <button>Shop Now </button>
+          <button className="secondary-btn">Category</button>
+        </div>
+
+        <div className="shopping">
+          <p>Also Available On</p>
+
+          <div className="brand-icons">
+            <img src="/images/amazon.png" alt="amazon-logo" />
+            <img src="/images/flipkart.png" alt="flipkart-logo" />
+          </div>
+        </div>
+      </div>
+      <div className="hero-image">
+        <img src="/images/hero-image.png" alt="hero-image" />
+      </div>
+    </main>
+  );
+};
+
+export default HeroSection;
